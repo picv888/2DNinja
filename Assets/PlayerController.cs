accd906e5ae7ab2b85c3ba64fbd108a7b2b96eea
Guanchao <274009365@qq.com>
2018-05-11 16:59:14 +0800

    2018年5月11日16:59:11

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerController : MonoBehaviour {
+    public float moveVelocity;//移动速度
+    public UnityEvent<float> onRun;//奔跑事件, 参数: float : 奔跑速度,
+    public UnityEvent onIdle;//
+                             // Use this for initialization
+    void Start() {
+    }
+
+    // Update is called once per frame
+    void Update() {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        transform.LookAt(Vector3.Lerp(transform.forward, transform.position + new Vector3(h, 0, v), 0.1f));
+        transform.position += Time.deltaTime * moveVelocity * new Vector3(h, 0, v);
+    }
+}
